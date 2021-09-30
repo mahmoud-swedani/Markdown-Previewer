74fbc565034148f3a2d749e2c2243060082517e4
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-30 06:00:22 +0300

    fix script to run in live server

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-import React, { Component } from "https://cdn.skypack.dev/react";
-import ReactDOM from "https://cdn.skypack.dev/react-dom";
-
 marked.setOptions({
   breaks: true,
   highlight: function (code) {
@@ -14,12 +11,12 @@ renderer.link = function (href, title, text) {
   return `<a target="_blank" href="${href}">${text}</a>`;
 };
 
-class Previewer extends Component {
+class Previewer extends React.Component {
     render() {
         return (
             <div className="Previewer card  col-xs-11 col-md-5 col-lg-5  lse" >
             <div className="card-body">
-              <div className="display-4"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+              <div className="display-4"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-eye" viewBox="0 0 16 16">
   <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
   <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
 </svg> Previewer</div><hr/>
@@ -36,15 +33,16 @@ class Previewer extends Component {
 }
 
 
-class Marked extends Component {
+class Marked extends React.Component {
     render() {
         return (
              <div className="Marked card" >
-            <div classname="card-body ">
+            <div className="card-body ">
 
-                   <div className="display-4 col-9"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-code " viewBox="0 0 16 16">
-          <path d="M5.854 4.854a.5.5 0 1 0-.708-.708l-3.5 3.5a.5.5 0 0 0 0 .708l3.5 3.5a.5.5 0 0 0 .708-.708L2.707 8l3.147-3.146zm4.292 0a.5.5 0 0 1 .708-.708l3.5 3.5a.5.5 0 0 1 0 .708l-3.5 3.5a.5.5 0 0 1-.708-.708L13.293 8l-3.147-3.146z"/>
-                      </svg> Editor<hr/>
+                   <div className="display-4 col-9">
+                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-code " viewBox="0 0 16 16">
+                       <path d="M5.854 4.854a.5.5 0 1 0-.708-.708l-3.5 3.5a.5.5 0 0 0 0 .708l3.5 3.5a.5.5 0 0 0 .708-.708L2.707 8l3.147-3.146zm4.292 0a.5.5 0 0 1 .708-.708l3.5 3.5a.5.5 0 0 1 0 .708l-3.5 3.5a.5.5 0 0 1-.708-.708L13.293 8l-3.147-3.146z"/>
+                    </svg> Editor<hr/>
                </div>
                 <textarea className="card txtara"  value={this.props.prev} id="editor" onChange={this.props.edit} />
               </div>
@@ -54,7 +52,7 @@ class Marked extends Component {
 }
 
 
-class Apps extends Component {
+class Apps extends React.Component {
 
     state = {
         content:placeholder,
